765e2796958434f5cfe6b4052775985ff1f756c5
glennlopez <glennlopez@gmail.com>
2017-01-27 05:08:17 -0800

    fixed bug

--- Ruby/rails/blog/app/controllers/posts_controller.rb
+++ Ruby/rails/blog/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update]
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
 	  @posts = Post.all.order("created_at DESC")
   end
