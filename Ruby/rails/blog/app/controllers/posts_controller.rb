f06b0555d4722c330c8d3e4bba4f0c17dbcab659
glennlopez <glennlopez@gmail.com>
2017-01-27 04:51:21 -0800

    added update controller

--- Ruby/rails/blog/app/controllers/posts_controller.rb
+++ Ruby/rails/blog/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update]
   def index
 	  @posts = Post.all.order("created_at DESC")
   end
@@ -17,8 +18,22 @@ class PostsController < ApplicationController
   end
 
   def show
-	  @post = Post.find(params[:id])
   end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
 
   private
 
--- Ruby/rails/blog/config/routes.rb
+++ Ruby/rails/blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'posts/index'
   resources :posts
+  #root 'posts#index' #place blog posts on root index 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
