9fb89e4f7faaeb22bcf29d2c2591608d88d0f5fd
Glenn <glennlopez@gmail.com>
2017-01-30 17:56:49 +0000

    added django files

---
+++ Python/django/Dev/cfehome/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pi/Github/Webdev.Playground/Python/django/Dev/cfehome/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
