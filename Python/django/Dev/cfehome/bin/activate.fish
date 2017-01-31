8ca49d7585a30670ea4f58ede05bb50681c4049e
glennlopez <glennlopez@gmail.com>
2017-01-31 07:12:55 -0800

    created development server

--- Python/django/Dev/cfehome/bin/activate.fish
+++ Python/django/Dev/cfehome/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/Github/Webdev.Playground/Python/django/Dev/cfehome"
+set -gx VIRTUAL_ENV "/home/glennlopez/Github/Webdev.Playground/Python/django/Dev/cfehome"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
